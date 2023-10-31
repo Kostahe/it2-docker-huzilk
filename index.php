285d132e9f6a6e134c658a029c7498c496d3decd
huzilk <huzilk@ssemi.cz>
2023-10-31 09:35:53 +0100

    php ukol 2 finished

--- index.php
+++ index.php
@@ -1,55 +1,57 @@
 <?php
-    $zapnuto = true;
-    $nazev="Kostja";
-    $cena=23.33;
-    $cislo=16;
-    $cislo2= 24;
 
-    $novaPromenna=$cislo.$cena."czk";
+$jmenoPrijmeni1 = 1;
+$jmenoPrijmeni2 = 1;
 
-    echo $zapnuto, "<br>";
+if ($jmenoPrijmeni1 == $jmenoPrijmeni2) {
+    echo "Konstantyn Huzil", "<br>";
+}
 
-    echo $nazev, "<br>";
-    echo $cena, "<br>";
-    echo $cislo, "<br>";
-    echo $novaPromenna, "<br>";
-    echo "<Img src='it2/images/skola.png'>", "<br>";
-    echo "\\ \$ ";
-
-    var_dump($cislo+$cislo2);
-    var_dump($cislo-$cislo2);
-    var_dump($cislo/$cislo2);
-    var_dump($cislo*$cislo2);
-
-    $cislo++;
-    var_dump($cislo);
-
-    $cislo--;
-    var_dump($cislo);
-
-    $cislo += $cislo2;
-    var_dump($cislo);
-
-    $cislo -= $cislo2;
-    var_dump($cislo);
-
-    $cislo *= $cislo2;
-    var_dump($cislo);
-
-    $cislo /= $cislo2;
-    var_dump($cislo);
-
-    var_dump($cislo == $cislo2);
-    var_dump($cislo != $cislo2);
-    var_dump($cislo > $cislo2);
-    var_dump($cislo < $cislo2);
-
-    var_dump($cislo && $cislo2);
-    var_dump($cislo && $cislo2);
-
-    var_dump(!$cislo && !$cislo2);
-    var_dump(!$cislo || $cislo2);
+if ($jmenoPrijmeni1 == $jmenoPrijmeni2) {
+    echo "Konstantyn","<br>";
+} else {
+    echo "Huzil", "<br>";
+}
 
 
 
+if($jmenoPrijmeni1 < $jmenoPrijmeni2) {
+    echo "Plati if", "<br>";
+} elseif($jmenoPrijmeni2 == $jmenoPrijmeni1) {
+    echo "Plati else if", "<br>";
+} else {
+    echo "Plate else", "<br>";
+}
+
+switch ($jmenoPrijmeni1) {
+    case 1:
+        echo "1";
+        break;
+    case 2:
+        echo "2";
+        break;
+    case 3:
+        echo "3";
+        break;
+    case 4:
+        echo "4";
+        break;
+    case 5:
+        echo "5";
+        break;
+    default:
+        echo "Neznama";
+        break;
+}
+
+$produkty = array("pocitace", "tiskarny", "konzole", "mobily", "tablety");
+$produkty["tiskarny"]["laserova"] = "HP";
+$produkty["tiskarny"]["laserova"] = "Canon";
+$produkty["pocitace"]["notebooky"] = "Lenovo";
+$produkty["pocitace"]["notebooky"] = "Acer";
+$produkty["mobily"]["Android"] = "Samsung";
+$produkty["mobily"]["IOS"] = "Apple";
+$produkty["mobily"]["Android"] = "Xiaomi";
+
+echo $produkty["mobily"]["Android"];
 
