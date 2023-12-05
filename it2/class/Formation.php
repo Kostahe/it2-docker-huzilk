d98d02dbcee20dc80e28c165b6f95d4953f3b37b
huzilk <huzilk@ssemi.cz>
2023-12-05 09:07:37 +0100

    Ukol 2 finished in php

--- it2/class/Formation.php
+++ it2/class/Formation.php
@@ -1,6 +1,10 @@
 <?php
 
 class Formation {
+    /**
+     * @param string $jmena
+     * @return array
+     */
     static function prepareUsers(string $jmena): array {
         $jmena = str_replace(":", ",", $jmena);
         $jmena = str_replace(";", ",", $jmena);
